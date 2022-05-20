3ec3f364fbf301c23da7db7ecb0287e950f936bf
aidenzegil <40921524+aidenzegil@users.noreply.github.com>
2022-05-20 13:28:02 -0400

    setup completed

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
-import './App.css';
+import "./App.css";
+import Projects from "./components/projects/Project";
+import Notes from "./components/notes/Notes";
+import Resume from "./components/resume/Resume";
+import Contact from "./components/contact/Contact";
 
 function App() {
   return (
     <div className="App">
+      <header className="Header">AidenZegil.com</header>
+      <div className="Container">
+        <Projects />
+        <Notes />
+        <Resume />
+        <Contact />
+      </div>
+      <footer className="Footer">Version: 0.0.1</footer>
     </div>
   );
 }
